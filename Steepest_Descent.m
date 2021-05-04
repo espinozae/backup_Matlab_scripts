60fd3b355d5388f6ef7ded68a4f90c158624ca7d
Erick Espinoza <erickeep@hotmail.com>
2021-05-04 12:12:16 -0700

    Update and rename Steepest-Descent.m to Steepest_Descent.m

--- Steepest_Descent.m
+++ Steepest_Descent.m
@@ -1,5 +1,5 @@
 % Tarea 5 Optimización
-% 23/11/18 Minimizar la funcion con Steepest-Descent
+% 23/11/18 Minimizar la funcion con Steepest_Descent
 clc
 close all
 % f(x1,x2) = x1-x2+2*(x1^2)+2*(x1*x2)+(x2^2) Ejemplo
